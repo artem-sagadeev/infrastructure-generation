c35b7722e119ad9880ea77ddaa15667abd3f8ed9
artem-sagadeev <sagadeev.artem2001@gmail.com>
2023-06-29 08:42:09 +0300

    final

--- src/App.ConsoleApp/Program.cs
+++ src/App.ConsoleApp/Program.cs
@@ -1,26 +1,39 @@
 ﻿using App.Common;
-using App.ConsoleApp;
 using App.DockerCompose;
 using App.Dockerfiles;
 using App.DotnetApps;
 
-const string defaultModelFilePath = "model1.json";
-const string defaultPathToPlaceFiles = "./";
-
-Console.WriteLine("Укажите путь до json-файла, содержащего модель информационной системы");
-var modelFilePath = Console.ReadLine();
-modelFilePath = string.IsNullOrEmpty(modelFilePath) ? defaultModelFilePath : modelFilePath;
-
-Console.WriteLine("Укажите путь для расположения сгенерированных файлов");
-var pathToPlaceFiles = Console.ReadLine();
-pathToPlaceFiles = string.IsNullOrEmpty(pathToPlaceFiles) ? defaultPathToPlaceFiles : pathToPlaceFiles;
-
-var configuration = new Configuration(pathToPlaceFiles);
-
-var softwareSystems = ModelReader.ReadModel(modelFilePath);
-foreach (var softwareSystem in softwareSystems)
+namespace App.ConsoleApp
 {
-    var solution = DotnetAppsGenerator.CreateDotnetApps(softwareSystem, configuration);
-    DockerfilesGenerator.CreateDockerfiles(solution, configuration);
-    DockerComposeGenerator.CreateDockerCompose(softwareSystem, solution, configuration);
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("---------- Start ----------");
+            
+            const string defaultModelFilePath = "model.json";
+            const string defaultPathToPlaceFiles = "./";
+            
+            var modelFilePath = args.Length > 0 ? args[0] : null;
+            modelFilePath = string.IsNullOrEmpty(modelFilePath) ? defaultModelFilePath : modelFilePath;
+            
+            var pathToPlaceFiles = args.Length > 1 ? args[1] : null;
+            pathToPlaceFiles = string.IsNullOrEmpty(pathToPlaceFiles) ? defaultPathToPlaceFiles : pathToPlaceFiles;
+
+            var configuration = new Configuration(pathToPlaceFiles);
+
+            var softwareSystems = ModelReader.ReadModel(modelFilePath);
+            foreach (var softwareSystem in softwareSystems)
+            {
+                var solution = DotnetAppsGenerator.CreateDotnetApps(softwareSystem, configuration);
+                DockerfilesGenerator.CreateDockerfiles(solution, configuration);
+                DockerComposeGenerator.CreateDockerCompose(softwareSystem, solution, configuration);
+            }
+            
+            Console.WriteLine("---------- End ----------");
+            Console.WriteLine();
+            Console.Write("Press any key...");
+            Console.ReadKey();
+        }
+    }
 }
